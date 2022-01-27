90981bd4a82b20179e083620918f6ef30fad0e8f
Rafael Bernardino <38199191+rafaelbds04@users.noreply.github.com>
2022-01-27 12:41:59 -0300

    Created basics operations

--- Desafio2-RafaelBernardino.cpp
+++ Desafio2-RafaelBernardino.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-char operacao;
+int operation;
 
 float firstValue, secondValue;
 
@@ -17,7 +17,39 @@ int main()
     cout << "Ola, bem vindo a calculadora arcaica" << endl;
     cout << "Digite qual das operacoes deseja fazer?" << endl;
     cout << "[1] Soma \n[2] Subtracao \n[3] Multiplicacao \n[4] Divisao \n[5] Resto Divisao \n[6] Raiz Quadrada \n[7] Fatorial" << endl;
-    cin >> operacao;
+    cin >> operation;
+
+    if (operation < 4 && operation > 0)
+    {
+        getInputs();
+    }
+
+    switch (operation)
+    {
+    case 1:
+        cout << "Resultado da soma: " << firstValue << " + " << secondValue << " = " << firstValue + secondValue << endl;
+        break;
+    case 2:
+        cout << "Resultado da subtracao:" << firstValue << " - " << secondValue << " = " << firstValue - secondValue << endl;
+        break;
+    case 3:
+        cout << "Resultado da multiplicacao: " << firstValue << " * " << secondValue << " = " << firstValue * secondValue << endl;
+        break;
+    case 4:
+        getInputs("numerador", "denominador");
+        if (secondValue == 0)
+        {
+            cout << "Nao e possivel dividir por zero" << endl;
+        }
+        else
+        {
+            cout << "Resultado da divisao: " << firstValue << " / " << secondValue << " = " << firstValue / secondValue << endl;
+        }
+        break;
+    default:
+        cout << "Operacao invalida" << endl;
+        break;
+    }
 
     return 0;
 }
