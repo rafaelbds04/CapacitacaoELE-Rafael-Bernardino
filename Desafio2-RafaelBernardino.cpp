d0df688ff3581e8e1d217a281fd5b3ece77be155
Rafael Bernardino <38199191+rafaelbds04@users.noreply.github.com>
2022-01-27 13:13:46 -0300

    Added factorial, square root and remainder

--- Desafio2-RafaelBernardino.cpp
+++ Desafio2-RafaelBernardino.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
+#include <math.h>
+
 using namespace std;
+int factorial(int value);
 int operation;
 
 float firstValue, secondValue;
 
 void getInputs(string firstValueName = "primeiro numero", string secondValueName = "segundo numero")
 {
-    cout << "Digite o " << firstValueName << endl;
+    cout << "Digite o " << firstValueName << ":" << endl;
     cin >> firstValue;
-    cout << "Digite o " << secondValueName << endl;
+    cout << "Digite o " << secondValueName << ":" << endl;
     cin >> secondValue;
 }
 
 int main()
 {
     cout << "Ola, bem vindo a calculadora arcaica" << endl;
-    cout << "Digite qual das operacoes deseja fazer?" << endl;
+    cout << "Digite qual das operacoes deseja fazer: " << endl;
     cout << "[1] Soma \n[2] Subtracao \n[3] Multiplicacao \n[4] Divisao \n[5] Resto Divisao \n[6] Raiz Quadrada \n[7] Fatorial" << endl;
     cin >> operation;
 
@@ -46,10 +49,43 @@ int main()
             cout << "Resultado da divisao: " << firstValue << " / " << secondValue << " = " << firstValue / secondValue << endl;
         }
         break;
+    case 5:
+        getInputs("numerador", "denominador");
+        cout << "Resto da divisao: " << firstValue << " % " << secondValue << " = " << (int)firstValue % (int)secondValue << endl;
+        break;
+    case 6:
+        cout << "Digite o radicando:" << endl;
+        cin >> firstValue;
+        if (firstValue < 0)
+        {
+            cout << "Não e possivel encontrar raiz real de um numero negativo" << endl;
+        }
+        else
+        {
+            cout << "Resultado da raiz de: " << firstValue << " = " << sqrt(firstValue) << endl;
+        }
+        break;
+    case 7:
+        cout << "Digite o numero no qual deseja descobrir o fatorial:" << endl;
+        cin >> firstValue;
+        cout << "Resultado fatorial de: " << firstValue << " = " << factorial(firstValue) << endl;
+        break;
     default:
         cout << "Operacao invalida" << endl;
         break;
     }
 
     return 0;
+}
+
+int factorial(int value)
+{
+    int result = value;
+    while (value > 1)
+    {
+        result *= (value - 1);
+        value -= 1;
+    }
+
+    return result;
 }
